184d646925ad2952bae62de404d24d42841b10b1
George MacRorie <me@georgemac.com>
2020-01-02 16:15:29 +0100

    chore(escape): remove unused nameEscapeBytes function

--- escape.go
+++ escape.go
@@ -112,42 +112,6 @@ func escapeBytes(dest *[]byte, b []byte) {
 	*dest = append(*dest, b...)
 }
 
-// Escape a measurement name
-func nameEscapeBytes(dest *[]byte, b []byte) {
-	if bytes.ContainsAny(b, nameEscapes) {
-		var r rune
-		for i, j := 0, 0; i < len(b); i += j {
-			r, j = utf8.DecodeRune(b[i:])
-			switch {
-			case r == '\t':
-				*dest = append(*dest, `\t`...)
-			case r == '\n':
-				*dest = append(*dest, `\n`...)
-			case r == '\f':
-				*dest = append(*dest, `\f`...)
-			case r == '\r':
-				*dest = append(*dest, `\r`...)
-			case r == ',':
-				*dest = append(*dest, `\,`...)
-			case r == ' ':
-				*dest = append(*dest, `\ `...)
-			case r == '\\':
-				*dest = append(*dest, `\\`...)
-			case r <= 1<<7-1:
-				*dest = append(*dest, byte(r))
-			case r <= 1<<11-1:
-				*dest = append(*dest, utf8len2|byte(r>>6), utf8bytex|byte(r)&utf8mask)
-			case r <= 1<<16-1:
-				*dest = append(*dest, utf8len3|byte(r>>12), utf8bytex|byte(r>>6)&utf8mask, utf8bytex|byte(r)&utf8mask)
-			default:
-				*dest = append(*dest, utf8len4|byte(r>>18), utf8bytex|byte(r>>12)&utf8mask, utf8bytex|byte(r>>6)&utf8mask, utf8bytex|byte(r)&utf8mask)
-			}
-		}
-		return
-	}
-	*dest = append(*dest, b...)
-}
-
 func stringFieldEscapeBytes(dest *[]byte, b []byte) {
 	if bytes.ContainsAny(b, stringFieldEscapes) {
 		var r rune
